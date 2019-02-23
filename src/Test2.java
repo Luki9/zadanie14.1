ba1adbf999adce2ea682dce4d0dcae5a87351c06
lukasz <robson27-r@hotmail.com>
2019-02-23 14:19:03 +0100

    w trakcie 2

--- src/Test2.java
+++ src/Test2.java
@@ -1,11 +1,27 @@
 import java.util.ArrayList;
+import java.util.Collection;
 
 class Test2 {
 
     public static void main(String[] args) {
 
-        ArrayList<Computer> computers = new ArrayList<>();
+        ArrayList<Computer> laptops = new ArrayList<>();
 
-        
+        Computer laptop1 = new Computer("Samsung",2.5,8);
+        Computer laptop2 = new Computer("Alien", 4.2,6);
+        Computer laptop3 = new Computer("Alien", 4.2,10);
+        Computer laptop4 = new Computer("Alien", 3.6,16);
+
+
+
+        laptops.add(laptop1);
+        laptops.add(laptop2);
+        laptops.add(laptop3);
+        laptops.add(laptop4);
+
+        System.out.println(laptops + "\n");
+
     }
+
+
 }
